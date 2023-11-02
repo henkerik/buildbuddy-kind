a2d37d5ba36695419b27b53b0c9c0827b432276d
Henk Erik van der Hoek <mail@henkerikvanderhoek.nl>
2023-11-02 20:45:24 +0100

    print logs

--- test.sh
+++ test.sh
@@ -5,5 +5,8 @@ set -e
 KIND=$1
 
 $KIND --version
-$KIND create cluster --verbosity 4
+$KIND create cluster --verbosity 4 || true
 $KIND get nodes
+
+echo "Printing logs..."
+docker logs kind-control-plane
