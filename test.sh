efba580cc10ec583683a6d9b5fe82af9e6c87bc9
Henk Erik van der Hoek <mail@henkerikvanderhoek.nl>
2023-11-02 21:32:48 +0100

    try sudo

--- test.sh
+++ test.sh
@@ -2,11 +2,16 @@
 
 set -e
 
+docker info
+
 KIND=$1
 
 $KIND --version
-$KIND create cluster --verbosity 4 || true
+sudo $KIND create cluster --verbosity 4 || true
 $KIND get nodes
+
+echo "docker ps --all"
+docker ps --all
 
 echo "Printing logs..."
 docker ps -a --format '{{json .ID}}' | xargs -I %s -- docker logs %s
